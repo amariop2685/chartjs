d083d1ff0ec11003841a508e922fc6f9c92b9f4a
Mario Acosta <acostap.marioa@gmail.com>
2023-11-24 14:15:53 -0300

    Chart.js - graficos con colores desde API

--- Ejercicio4/assets/js/index.js
+++ Ejercicio4/assets/js/index.js
@@ -1,19 +1,40 @@
 console.log("entro a index.js")
-
+const tbody = document.getElementById('tbody');
 fetch("https://dev4humans.com.mx/api/Clases/ventas_libros")
 .then(response => response.json())
 .then(datosApi => {
   console.log(datosApi);
   const ctx = document.getElementById('myChart');
+
+  const labels = datosApi.data.labels;
+  const data = datosApi.data.data;
   
+
+  //Creación de graficas
     new Chart(ctx, {
       type: 'doughnut',
       data: {
-        labels: datosApi.data.labels,
+        labels: labels,
         datasets: [{
-          label: '# of votes',
-          data: datosApi.data.data,
-          borderWidth: 1
+          label: 'Promedio de ventas diarias',
+          data: data,
+          borderWidth: 1,
+          borderColor:[
+            "#3677D4",
+            "#5FD436",
+            "#D436C5",
+            "#D43636",
+            "#D1D436"
+            
+          ],
+
+          backgroundColor:[
+            "#3677D4",
+            "#5FD436",
+            "#D436C5",
+            "#D43636",
+            "#D1D436"
+          ]
         }]
       },
       options: {
@@ -24,7 +45,35 @@ fetch("https://dev4humans.com.mx/api/Clases/ventas_libros")
         }
       }
     });
-});
+    //Creación de datos de tabla
 
+    tbody.innerHTML = "";
+
+    labels.forEach((label, index) => {
+      console.log(index);
+      
+      
+      tbody.innerHTML += `
+      <tr ${data[index] > 50 ? 'class="table-danger fw-bold"' : ''}>
+      <th>${index + 1}</th>
+      <th>${label}</th>
+      <th>${data[index]}</th>
+      </tr>
+    `;
+        // const tr = document.createElement("tr");
+        // if(data[index]>50){
+        //   tr.classList.add("table-danger");
+        // }
+       
+        
+        // tr.innerHTML += `
+        //     <td>${index + 1}</td>
+        //     <td>${label}</td>
+        //     <td>${data[index]}</td>
+        //     `;
+        //     tbody.appendChild(tr);
+    });
+
+  });
 
     
